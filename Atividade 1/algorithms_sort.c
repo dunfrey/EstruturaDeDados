6534a76a2952f7704330da1db2d4e10a35c0323f
Dunfrey Pires <dunfrey@gmail.com>
2018-08-26 14:13:39 -0300

    atualização do código

--- Atividade 1/algorithms_sort.c
+++ Atividade 1/algorithms_sort.c
@@ -23,17 +23,17 @@ void selection_sort(int * array, int tam_arr){
 
 void insertion_sort(int * array, int tam_arr){
 
-    for(int i = 1; i < tam_arr; i++)
+    for(int j = 1; j < tam_arr; j++)
     {
-        int min_atual   = array[i];
-        int j = i -1;
+        int chave = array[j];
+        int i = j-1;
 
-        while (j >= 0 && min_atual < array[j]) {
-            array[j+1] = array[j];
-            j--;
+        while (i >= 0 && array[i] > chave) {
+            array[i+1] = array[i];
+            i--;
         }
 
-        array[j+1] = min_atual;
+        array[i+1] = chave;
     }
 }
 
@@ -42,49 +42,6 @@ void print_ord_num(int array[], int tam_arr){
     for(int loop = 0; loop < tam_arr; loop++)
         printf("%d ", array[loop]);
     printf("\n\n");
-}
-
-char dir[] = "./instancias-num/";
-void solve_name_file(char * file){
-    strcat(dir, file);
-}
-
-int count_inst(){
-    FILE *stream;
-	char *line = NULL;
-	size_t len = 0, read;
-    int i = -1;
-
-    stream = fopen (dir , "r");
-    if (dir == NULL) perror ("Error opening file");
-    else {
-        while ((read = getline(&line, &len, stream)) != -1) {
-            i++;
-        }
-    }
-
-	free(line);
-	fclose(stream);
-    return i;
-}
-
-void copy_inst(int * array){
-    FILE *stream;
-	char *line = NULL;
-	size_t len = 0, read;
-    int i = -1;
-
-    stream = fopen (dir , "r");
-    if (dir == NULL) perror ("Error opening file");
-    else {
-        while ((read = getline(&line, &len, stream)) != -1) {
-            i++;
-            array[i] = (int) atoi(line);
-        }
-    }
-
-	free(line);
-	fclose(stream);
 }
 
 int * embaralhar(int * array, int vetSize){
@@ -99,49 +56,65 @@ int * embaralhar(int * array, int vetSize){
     return array;
 }
 
-void ordenar(int * array){
+void ordenar(int * array, int tam_array){
     clock_t t1, t2, t3;
     t1 = clock();
-
-    int tam_array = count_inst();
     
-    selection_sort(array, tam_array);
+    insertion_sort(array,tam_array);
+    print_ord_num(array, tam_array);
     t2 = clock() - t1;
+    printf("\ninsertion_sort() levou %f segundos para ordenar\n", ((double)t2)/CLOCKS_PER_SEC);
 
-    insertion_sort(embaralhar(array, tam_array),tam_array);
+    printf("\n\n\n-----------------------------------------------\n\n\n");
+
+    selection_sort(embaralhar(array, tam_array), tam_array);
+    print_ord_num(array, tam_array);
     t3 = clock() - t2;
+    printf("\nselection_sort() levou %f segundos para ordenar\n", ((double)t3)/CLOCKS_PER_SEC);
 
-    printf("\nselection_sort() levou %f segundos para ordenar", ((double)t2)/CLOCKS_PER_SEC);
-    printf("\ninsertion_sort() levou %f segundos para ordenar\n\n", ((double)t3)/CLOCKS_PER_SEC);
-    //print_ord_num(array, count_inst());
+}
+
+char dir[] = "./instancias-num/";
+void name_file(char * file){
+    strcat(dir, file);
+}
+
+void instances(){
+    FILE *stream;
+	char *line = NULL;
+	size_t len = 0, read;
+    int i = 0;
+
+    stream = fopen (dir , "r");
+    
+    if (dir == NULL){
+        int array[1];
+    }
+    else {
+        read = getline(&line, &len, stream);
+        int k_inst = atoi(line);
+        int array[k_inst];
+        while ((read = getline(&line, &len, stream)) != -1) {
+            array[i] = atoi(line);
+            i++;
+        }
+        //printf("%d",i);
+        ordenar(array, i);
+    }
+
+    free(line);
+    fclose(stream);
+
 }
 
 void file_process(char * file){
-    
-    solve_name_file(file);   
-    int array[count_inst()];
-    copy_inst(array);
-    ordenar(array);
-    
-}
-
-void without_file_process(int argc, char **argv){
-    int tam_arr = (int)argc - 1;
-    int array[tam_arr];
-    for(int pos_arr = 0; pos_arr < tam_arr; pos_arr++)
-    {
-        array[pos_arr] = atoi(argv[pos_arr+1]);
-    }
-    
-    ordenar(array);
+    name_file(file);
+    instances();
 }
 
 int main( int argc, char **argv ){
     if(argc == 2){
         file_process(argv[1]);
-    } 
-    else if(argc > 2){
-        without_file_process(argc, argv);      
     } else {
         puts("ERRO NA ENTRADA");
     }
